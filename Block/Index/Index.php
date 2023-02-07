6a5f0dc01a06f3997cf072dfc12a17133129e0de
vishal kumar <vishalkumar150520@gmail.com>
2023-02-07 17:24:27 +0530

    hello

--- Block/Index/Index.php
+++ Block/Index/Index.php
@@ -1,15 +1,20 @@
 <?php
-
 namespace Bluethink\Verify\Block\Index;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
-	{
-		parent::__construct($context);
-	}
-	public function msg()
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Bluethink\Verify\Helper\Data $helper,
+        array $data = []
+    ) {
+        $this->helper = $helper;
+        parent::__construct($context, $data);
+    }
+
+    public function getBlogs()
     {
-        echo "hello";
+        $data = $this->helper->getConfigValue();
+        return $data;
     }
 }
